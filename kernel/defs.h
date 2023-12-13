e1830ccec6bf97088d91630025ac619849ad8133
sstshering <sstshering@miners.utep.edu>
2023-12-13 01:14:17 -0700

    hw5

--- kernel/defs.h
+++ kernel/defs.h
@@ -182,7 +182,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
-int             mapvpages(pagetable_t, unit64, unint64);
+int             mapvpages(pagetable_t, uint64, uint64);
 
 // plic.c
 void            plicinit(void);
